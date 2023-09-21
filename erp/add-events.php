eee6177a40c36aba71231597de8bc192aeb5144a
saisivamani <sivamani_csd@srkrec.edu.in>
2023-09-21 14:39:00 +0530

    Changed To PHP

---
+++ erp/add-events.php
@@ -0,0 +1,127 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
+        <title>Preskool - Events</title>
+		
+		<!-- Favicon -->
+        <link rel="shortcut icon" href="assets/img/favicon.png">
+	
+		<!-- Fontfamily -->
+		<link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;0,500;0,700;0,900;1,400;1,500;1,700&display=swap" rel="stylesheet">
+		
+		<!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="assets/plugins/bootstrap/css/bootstrap.min.css">
+		
+		<!-- Feathericon CSS -->
+        <link rel="stylesheet" href="assets/plugins/feather/feather.css">
+		
+		<!-- Pe7 CSS -->
+		<link rel="stylesheet" href="assets/plugins/icons/flags/flags.css">
+		
+		<!-- Datepicker CSS -->
+		<link rel="stylesheet" href="assets/css/bootstrap-datetimepicker.min.css">
+		
+		<!-- Fontawesome CSS -->
+		<link rel="stylesheet" href="assets/plugins/fontawesome/css/fontawesome.min.css">
+		<link rel="stylesheet" href="assets/plugins/fontawesome/css/all.min.css">
+		
+		<!-- Main CSS -->
+        <link rel="stylesheet" href="assets/css/style.css">
+    </head>
+    <body>
+		
+		<!-- Main Wrapper -->
+        <div class="main-wrapper">
+		
+			<?php include'header.php' ?>
+
+			
+			<!-- Page Wrapper -->
+            <div class="page-wrapper">
+                <div class="content container-fluid">
+				
+					<!-- Page Header -->
+					<div class="page-header">
+						<div class="row align-items-center">
+							<div class="col">
+								<h3 class="page-title">Add Event</h3>
+								<ul class="breadcrumb">
+									<li class="breadcrumb-item"><a href="event.html">Events</a></li>
+									<li class="breadcrumb-item active">Add Event</li>
+								</ul>
+							</div>
+						</div>
+					</div>
+					<!-- /Page Header -->
+				
+					<div class="row">
+						<div class="col-sm-12">
+						
+							<div class="card">
+								<div class="card-body">
+									<form>
+										<div class="row">
+											<div class="col-12">
+												<h5 class="form-title"><span>Event Information</span></h5>
+											</div>
+											<div class="col-12 col-sm-4">
+												<div class="form-group local-forms">
+													<label>Event ID <span class="login-danger">*</span></label>
+													<input type="text" class="form-control">
+												</div>
+											</div>
+											<div class="col-12 col-sm-4">
+												<div class="form-group local-forms">
+													<label>Event Name <span class="login-danger">*</span></label>
+													<input type="text" class="form-control">
+												</div>
+											</div>
+											<div class="col-12 col-sm-4">
+												<div class="form-group local-forms calendar-icon">
+													<label>Event Date <span class="login-danger">*</span></label>
+													<input class="form-control datetimepicker" type="text"  placeholder="DD-MM-YYYY" >
+												</div>
+											</div>
+											<div class="col-12">
+												<div class="student-submit">
+													<button type="submit" class="btn btn-primary">Submit</button>
+												</div>
+											</div>
+										</div>
+									</form>
+								</div>
+							</div>
+							
+						</div>					
+					</div>					
+				</div>
+				
+			</div>
+			<!-- /Page Wrapper -->
+			
+        </div>
+		<!-- /Main Wrapper -->
+		
+		<!-- jQuery -->
+        <script src="assets/js/jquery-3.6.0.min.js"></script>
+		
+		<!-- Feather Icon JS -->
+		<script src="assets/js/feather.min.js"></script>
+		
+		<!-- Bootstrap Core JS -->
+        <script src="assets/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+		
+		<!-- Slimscroll JS -->
+		<script src="assets/plugins/slimscroll/jquery.slimscroll.min.js"></script>
+		
+		<!-- Datepicker Core JS -->
+		<script src="assets/plugins/moment/moment.min.js"></script>
+		<script src="assets/js/bootstrap-datetimepicker.min.js"></script>
+
+		<!-- Custom JS -->
+		<script src="assets/js/script.js"></script>
+		
+    </body>
+</html>
