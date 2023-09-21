d725292ca22915a31316eef5fa2a1a79e3f09621
saisivamani <sivamani_csd@srkrec.edu.in>
2023-09-21 15:32:53 +0530

    Demo And Some Other Minor Changes Done

--- erp/header.php
+++ erp/header.php
@@ -193,7 +193,7 @@ echo'
 								<a href="time-table.php"><i class="fas fa-table"></i> <span>Time Table</span></a>
 							</li>
 							<li> 
-								<a href="library.php"><i class="fas fa-book"></i> <span>Library</span></a>
+								<a href="demo.php"><i class="fas fa-book"></i> <span>Demo</span></a>
 							</li>
 							
 							
